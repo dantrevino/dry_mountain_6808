0cc081f348c62a736222766f23dd772b93a7aa1b
Dan Trevino <dantrevino@gmail.com>
2017-01-09 00:41:55 -0800

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main () {
   runApp(
     new Center(
-      child: new Text("Hello Word")
+      child: new Padding (
+        padding: const EdgeInsets.only(left: 128.0),
+        child: new Text("Hello Word")
+      )
     )
   );
  }
